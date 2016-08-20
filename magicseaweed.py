4fdc1e7e004a6df5888121a4f21ca857f9864af2
Thomas BLANQUET <thomas.blanquet@epitech.eu>
2016-08-20 16:10:01 +0200

    using localTime

--- magicseaweed.py
+++ magicseaweed.py
@@ -1,4 +1,6 @@
 import requests
+import calendar
+from datetime import datetime, timedelta
 
 
 class Magicseaweed():
@@ -8,7 +10,17 @@ class Magicseaweed():
         self.api_url = "http://magicseaweed.com/api/{0}/forecast"
         self.api_url = self.api_url.format(api_key)
 
+    def near_report(self, time):
+        while time.hour % 3 != 0:
+            time = time + timedelta(hours=1)
+        return datetime(time.year, time.month, time.day, time.hour, 0, 0)
+
     def get_forecast_at_spot(self, spot_id, local_time='None', units_mesures='eu'):
         data = requests.get('{0}?spot_id={1}&units={2}'.format(self.api_url, spot_id, units_mesures))
         data = data.json()
-        return data
+        timestamp = calendar.timegm(self.near_report(local_time).timetuple())
+        for forecast in data:
+            if forecast['localTimestamp'] == timestamp:
+                return forecast
+        return None
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
+from datetime import datetime
 from magicseaweed import Magicseaweed
 
 magicseaweed = Magicseaweed(os.environ['MSW_KEY'])
-forecast = magicseaweed.get_forecast_at_spot('1556')
+forecast = magicseaweed.get_forecast_at_spot('1556', datetime.utcnow())
