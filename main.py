a63310d6b789054b0d5d1b883f7b743a76a7cc08
thomas-blanquet <thom.blanquet@gmail.com>
2016-08-20 16:14:51 +0200

    pprint result

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import pprint
 from datetime import datetime
 from magicseaweed import Magicseaweed
 
 magicseaweed = Magicseaweed(os.environ['MSW_KEY'])
 forecast = magicseaweed.get_forecast_at_spot('1556', datetime.utcnow())
+pprint.pprint(forecast)
